37bb761b875329ab33a5e434d9a2d01e70dbe877
Conor Hermer <conorhermer@gmail.com>
2017-11-01 03:13:53 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   
+  resources :line_items
+  resources :carts
   resources :products
   get 'static_pages/home'
 
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'products#index'
-
+  #root 'products#index'
+  root 'store#index'
+  
 end
