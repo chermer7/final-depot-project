5a30d478187704ac61ffd8251b0ea8ec7e963f8f
Conor Hermer <conorhermer@gmail.com>
2017-11-16 23:22:50 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
   
   
+  get 'admin/index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+    
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+
   resources :users
+  resources :orders
   resources :line_items
   resources :carts
+  get 'store/index'
+
   resources :products
   get 'static_pages/home'
 
@@ -13,7 +30,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #root 'products#index'
   root 'store#index'
-  
+
 end
